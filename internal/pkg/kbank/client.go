8102c973cca01cd8beff60e394cfecb0b84221af
Osacn thakrit Ratchakote <osacn_thakrit.ratchakote@krungthai.com>
2026-03-20 17:04:20 +0700

    fix gener 2

--- internal/pkg/kbank/client.go
+++ internal/pkg/kbank/client.go
@@ -100,7 +100,7 @@ func (c *Client) GetAccessToken() (string, error) {
 	return tokenResp.AccessToken, nil
 }
 
-func (c *Client) GenerateThaiQR(requestUserID, amount string, reference1, reference2 string) (*QRResponse, error) {
+func (c *Client) GenerateThaiQR(requestUserID, bookingID, amount string, reference1, reference2 string) (*QRResponse, error) {
 	token, err := c.GetAccessToken()
 	if err != nil {
 		return nil, err
